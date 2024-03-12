97afa7e51b1b12b04d863a3c27892b6382ffd078
PYE <pye.edm@gmail.com>
2024-03-12 20:57:23 +0100

    Export WavParameters in output module

--- src/glyphtune/output/__init__.py
+++ src/glyphtune/output/__init__.py
@@ -1,6 +1,6 @@
 """Audio output."""
 
 from glyphtune.output.stream import Stream
-from glyphtune.output.file import write_wav
+from glyphtune.output.file import WavParameters, write_wav
 
-__all__ = ["Stream", "write_wav"]
+__all__ = ["Stream", "WavParameters", "write_wav"]
